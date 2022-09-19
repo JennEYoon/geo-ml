fbc2998f16f9940dc3b695dc14df4036bda668ed
Jennifer E. Yoon <jenneyoon@gmail.com>
2022-09-18 23:20:41 -0400

    Update and rename rainforest/gist/code_snippets.txt to rainforest/snippets/code_snippets.py

---
+++ rainforest/snippets/code_snippets.py
@@ -0,0 +1,19 @@
+# code snippets used in rainforest, collect here
+
+### loading dataset, untar data, pathlib  
+
+
+### os.walk and 
+
+
+### show image Matplotlib and PIL  
+
+
+### json Area of Interest selector 
+
+
+
+### export trained module, load pretrained module.  
+
+
+
